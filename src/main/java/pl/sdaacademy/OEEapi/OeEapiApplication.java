4ce936c4e7456bd90dec1075604ea4fc5a7a4929
Comm <IzaBelaMa2>
2021-02-20 13:21:22 +0100

    init(crrs): init project

---
+++ src/main/java/pl/sdaacademy/OEEapi/OeEapiApplication.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.OEEapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OeEapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OeEapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdaacademy/OEEapi/OeEapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.OEEapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OeEapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
